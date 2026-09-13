25444f860848ea2b4311b65290df35bd0d5346c8
dell <dell@DESKTOP-9VO4S6K>
2026-09-13 11:23:01 +0530

    commited

---
+++ src/test/java/demo/Demotest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demotest {
+	
+	
+  @Test(groups = "smoke")
+  public void test() {
+	  System.out.println("add");
+  }
+  
+  
+}
