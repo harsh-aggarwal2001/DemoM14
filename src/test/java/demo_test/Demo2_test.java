091e4591f9510dc3afe8e0bc082298df6896bed3
dell <dell@DESKTOP-9VO4S6K>
2026-09-13 12:17:06 +0530

    commit2

---
+++ src/test/java/demo_test/Demo2_test.java
@@ -0,0 +1,11 @@
+package demo_test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2_test {
+  @Test
+  public void test() {
+	  Reporter.log("hello , it is github demo test",true);
+  }
+}
